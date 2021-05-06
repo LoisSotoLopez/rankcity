ec872b59cc1fdd9fd7aa2302b0f93539ba7010c4
Lois Soto <loissotolopez@gmail.com>
2021-05-05 17:30:48 -0700

    feat(#2): Add login components with coroutines
    Closes #2

--- RankCityApp/app/src/main/java/com/apm2021/rankcity/LoginActivity.kt
+++ RankCityApp/app/src/main/java/com/apm2021/rankcity/LoginActivity.kt
@@ -1,35 +1,233 @@
 package com.apm2021.rankcity
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.json.JSONException
+import org.json.JSONObject
+import org.xml.sax.Parser
+import java.io.BufferedReader
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 
 class LoginActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        setup()
+        session()
+    }
+
+    private fun setup() {
+
+        val nameInputText = findViewById<TextInputEditText>(R.id.UserEmailInputText);
+        val passInputText = findViewById<TextInputEditText>(R.id.PasswordInputText);
         // Pulsar boton login nos lleva al inicio de la app
         val loginButton = findViewById<Button>(R.id.loginButton);
+
         // set on-click listener
         loginButton.setOnClickListener {
-            Toast.makeText(this, "Login correct", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if (nameInputText.text?.isNotEmpty() == true && passInputText.text?.isNotEmpty() == true) {
+                loginCoroutine(nameInputText.text.toString(), passInputText.text.toString());
+            } else {
+                Toast.makeText(this, "Missing User/Password", Toast.LENGTH_SHORT).show()
+            }
         }
 
         val registerButton = findViewById<Button>(R.id.registerButton);
         // set on-click listener
         registerButton.setOnClickListener {
-            Toast.makeText(this, "Registered", Toast.LENGTH_SHORT).show()
+            if (nameInputText.text?.isNotEmpty() == true && passInputText.text?.isNotEmpty() == true) {
+                signInCoroutine(nameInputText.text.toString(),passInputText.text.toString())
+            } else {
+                Toast.makeText(this, "User/Password missing", Toast.LENGTH_SHORT).show()
+            }
         }
 
+
         val loginButtonGoogle = findViewById<Button>(R.id.loginButtonGoogle);
+
         // set on-click listener
         loginButtonGoogle.setOnClickListener {
-            Toast.makeText(this, "Login with GOOGLE", Toast.LENGTH_SHORT).show()
+            googleLoginCoroutine(this)
         }
+    }
+
+
+    private fun signInCoroutine(name: String, pass: String){
+        GlobalScope.launch {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    name,
+                    pass).addOnCompleteListener {
+
+                if (it.isSuccessful && postUser(name)) {
+                    showMain(it.result?.user?.email ?: "", ProviderType.BASIC )
+                } else {
+                    showAlert("No se pudo registrar")
+                }
+            }
+        }
+    }
+
+    private fun googleLoginCoroutine(activity: Activity) {
+        GlobalScope.launch {
+            val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build()
+
+            val googleClient = GoogleSignIn.getClient(activity, googleConf)
+            googleClient.signOut()
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+    }
+
+    private fun loginCoroutine(name: String, pass: String) {
+        GlobalScope.launch {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                name,
+                pass
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val triple = getUser(name); // Retrieve required info here
+                    showMain(it.result?.user?.email ?: "", ProviderType.BASIC)
+                } else {
+                    showAlert("No se pudo loguear")
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val loginLayout = findViewById<LinearLayout>(R.id.loginLayout);
+        loginLayout.visibility = View.VISIBLE
+    }
+
+    private fun session() {
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+        val loginLayout = findViewById<LinearLayout>(R.id.loginLayout);
+
+        if (email != null && provider != null) {
+            loginLayout.visibility = View.INVISIBLE
+            showMain(email, ProviderType.valueOf(provider))
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showMain(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                showAlert( "Falló la comunicación con Firebase")
+                            }
+                        }
+                }
+            } catch(e: ApiException) {
+
+            }
+        }
+    }
+
+    private fun getUser(userid: String): Triple<Any, Any, Any> {
+        val result = URL("http://localhost:5000/users/" + userid).readText()
+        val jsonObject = JSONObject(result)
+        try {
+            return Triple(jsonObject.get("username"), jsonObject.get("name"), jsonObject.get("email"))
+        } catch (e: JSONException) {
+            return Triple("null","null","null")
+        }
+    }
+
+    private fun postUser(userid: String) : Boolean{
+        val conn = URL("http://localhost:5000/users/" + userid).openConnection() as HttpURLConnection
+        conn.requestMethod = "POST"
+        conn.connectTimeout = 300000
+        conn.doOutput = true
+
+        val message = " {\n" +
+                "                \"username\": " + userid +",\n" +
+                "                \"name\": " + userid +",\n" +
+                "                \"email\": " + userid +"\n" +
+                "            }"
+        val postData: ByteArray =
+                message.toByteArray(Charset.forName("UTF-8"))
+
+        conn.setRequestProperty("charset", "utf-8")
+        conn.setRequestProperty("Content-length", postData.size.toString())
+        conn.setRequestProperty("Content-Type", "application/json")
+
+        try {
+            val outputStream: DataOutputStream = DataOutputStream(conn.outputStream)
+            outputStream.write(postData)
+            outputStream.flush()
+        } catch (exception: Exception) {
+
+        }
+
+        return (conn.responseCode == HttpURLConnection.HTTP_OK)
+    }
+
+    private fun showAlert(msg: String) {
+        //TODO check if AlertDialog is of proper type
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(msg)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showMain(email: String, provider: ProviderType) {
+        val mainIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(mainIntent)
     }
 }
--- RankCityApp/app/src/main/java/com/apm2021/rankcity/MainActivity.kt
+++ RankCityApp/app/src/main/java/com/apm2021/rankcity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.apm2021.rankcity
 
+import android.content.Context
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,11 +11,29 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+enum class ProviderType {
+    BASIC,
+    GOOGLE
+}
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Saving auth data
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider = bundle?. getString("provider")
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+        //TODO: clear prefs entries for email and provider once app properly closes
+
+        Toast.makeText(this, "Email: "+email+" Provider: "+provider, Toast.LENGTH_SHORT).show()
 
         // Llamada a la barra de navegacion
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
