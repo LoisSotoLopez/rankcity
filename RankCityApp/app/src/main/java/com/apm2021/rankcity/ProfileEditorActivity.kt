0cfe6bc00bb955b77383b74ef45a1a043a4efa43
Lois Soto <loissotolopez@gmail.com>
2021-06-10 00:35:39 -0700

    feat(#16): Add profile edition after signing in
    Closes #16

--- RankCityApp/app/src/main/java/com/apm2021/rankcity/ProfileEditorActivity.kt
+++ RankCityApp/app/src/main/java/com/apm2021/rankcity/ProfileEditorActivity.kt
@@ -4,12 +4,14 @@ import android.Manifest
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.ContentValues
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.Editable
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -25,15 +27,16 @@ class ProfileEditorActivity : AppCompatActivity() {
     var photo: Uri? = null
     lateinit var imgPhoto: ImageView
     lateinit var cameraButton: FloatingActionButton
+    private var dialogOut : Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_editor)
 
-        val cameraButton = findViewById<Button>(R.id.editProfileEditImgButton) as FloatingActionButton
-        cameraButton.setOnClickListener {
-            dialog()
-        }
+        val bundle = intent.extras
+        val email = bundle?.getString("email") ?: ""
+        var mandatory = bundle?.getBoolean( "mandatory") ?: false
+
 
         val nicknameInputText = findViewById<TextInputEditText>(R.id.editNickInputText)
         // TODO asign val from backend
@@ -44,148 +47,79 @@ class ProfileEditorActivity : AppCompatActivity() {
         val acceptButton = findViewById<Button>(R.id.acceptProfileEditionButton)
         acceptButton.setOnClickListener {
             // TODO either add image parameter or select context values from edit_users scope
-            edit_user(nicknameInputText, hometownInputText)
+            accept(nicknameInputText, hometownInputText, email,  mandatory)
         }
 
         val cancelButton = findViewById<Button>(R.id.cancelProfileEditionButton)
         cancelButton.setOnClickListener {
-            cancel(nicknameInputText, hometownInputText)
+            cancel(nicknameInputText, hometownInputText, email, mandatory)
         }
 
     }
 
-    private fun cancel(nicknameInput: TextInputEditText, hometownInputText: TextInputEditText){
-        val mainIntent = Intent(this, MainActivity::class.java)
-        startActivity(mainIntent)
+    private fun cancel(
+        nicknameInput: TextInputEditText,
+        hometownInput: TextInputEditText,
+        email: String,
+        mandatory: Boolean){
+        if (!mandatory) {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        } else {
+            var editNick : String = nicknameInput.text.toString()
+            var hometown : String = hometownInput.text.toString()
+            if (editNick == null) editNick = email
+            if (hometown == null) hometown = ""
+            showCancelAlert(
+                "¿Estas seguro de que quieres cancelar?\n" +
+                        "Se usará tu correo como nombre de usuario.",
+                    nicknameInput,
+                    hometownInput,
+                    email)
+        }
     }
 
-    private fun edit_user(nicknameInput: TextInputEditText, hometownInputText: TextInputEditText){
-        if (nicknameInput.text?.isNotEmpty() == true && hometownInputText.text?.isNotEmpty() == true) {
-            // TODO handle profile image
+    private fun accept(
+        nicknameInput: TextInputEditText,
+        hometownInputText: TextInputEditText,
+        email: String,
+        mandatory: Boolean)
+    {
+        if ( (nicknameInput.text?.isNotEmpty() == true && hometownInputText.text?.isNotEmpty() == true) || !mandatory) {
             // TODO store in backend
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
         } else {
-            Toast.makeText(this, "Missing Fields", Toast.LENGTH_SHORT).show()
+            showAlert("Faltan algunos campos")
         }
     }
 
-    //Al pulsar botón para abrir cámara comprobamos permisos
-    private fun openCameraClick(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if (this.let {
-                    ContextCompat.checkSelfPermission(
-                        it,
-                        Manifest.permission.CAMERA
-                    )
-                } == PackageManager.PERMISSION_DENIED || this.let {
-                    ContextCompat.checkSelfPermission(
-                        it,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE
-                    )
-                } == PackageManager.PERMISSION_DENIED){
-                val cameraPermits = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                requestPermissions(cameraPermits, REQUEST_CAMERA)
-            }else{
-                openCamera()
-            }
-        }else{
-            openCamera()
-        }
-    }
-
-    //Abrir camara de móvil
-    private fun openCamera(){
-        val value = ContentValues()
-        value.put(MediaStore.Images.Media.TITLE, "Nueva imagen")
-        photo =
-            this.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, value)
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photo)
-        startActivityForResult(cameraIntent, REQUEST_CAMERA)
-    }
-
-    //Al pulsar botón para abrir galería comprobamos permisos
-    private fun openGalleryClick(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (this.let {
-                    ContextCompat.checkSelfPermission(
-                        it,
-                        Manifest.permission.READ_EXTERNAL_STORAGE
-                    )
-                } == PackageManager.PERMISSION_DENIED){
-                val archivePermits = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                requestPermissions(archivePermits, REQUEST_GALLERY)
-            }else{
-                showGallery()
-            }
-        }else{
-            showGallery()
-        }
-    }
-
-    //Abrir galería de fotos del móvil
-    private fun showGallery(){
-        val galleryIntent = Intent(Intent.ACTION_PICK)
-        galleryIntent.type = "image/*"
-        startActivityForResult(galleryIntent, REQUEST_GALLERY)
-
-    }
-
-    //Diálogo para elegir si abrir cámara o galería
-    private fun dialog() {
-        val opciones = arrayOf<CharSequence>("Abrir cámara", "Abrir galería", "Cancelar")
-        val alertOpciones = AlertDialog.Builder(this)
-        alertOpciones.setTitle("Seleccione una Opción")
-        alertOpciones.setItems(
-            opciones
-        ) { dialogInterface, i ->
-            if (opciones[i] == "Abrir cámara") {
-                openCameraClick()
-            } else {
-                if (opciones[i] == "Abrir galería") {
-                    openGalleryClick()
-                } else {
-                    dialogInterface.dismiss()
-                }
-            }
-        }
-        alertOpciones.show()
-    }
-
-    //Establecer foto de perfil cogiendo la foto de la cámara o de la galería
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_GALLERY){
-            imgPhoto.setImageURI(data?.data)
-        }
-        if(resultCode == Activity.RESULT_OK && requestCode == REQUEST_CAMERA){
-            imgPhoto.setImageURI(photo)
-        }
-    }
-
-    //Comprobar si se aceptan permisos
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
+    private fun showCancelAlert(
+        msg: String,
+        nicknameInput: TextInputEditText,
+        hometownInputText: TextInputEditText,
+        email: String,
     ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            REQUEST_GALLERY ->{
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    showGallery()
-                }else{
-                    Toast.makeText(this, "No se puede abrir la galería", Toast.LENGTH_SHORT).show()
-                }
-            }
-            REQUEST_CAMERA ->{
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    openCamera()
-                }else{
-                    Toast.makeText(this, "No se puede abrir la cámara", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+        //TODO check if AlertDialog is of proper type
+        val builder = androidx.appcompat.app.AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(msg)
+        builder.setPositiveButton("Aceptar", DialogInterface.OnClickListener {dialog, id ->
+            this.accept(nicknameInput, hometownInputText, email, false)
+        })
+        builder.setNegativeButton("Cancelar", null)
+        val dialog: androidx.appcompat.app.AlertDialog = builder.create()
+        dialog.show()
     }
+
+    private fun showAlert(msg: String) {
+        //TODO check if AlertDialog is of proper type
+        val builder = androidx.appcompat.app.AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage(msg)
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: androidx.appcompat.app.AlertDialog = builder.create()
+        dialog.show()
+    }
+
 }
